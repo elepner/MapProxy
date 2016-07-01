18866d2fa1c96b595e99f7172776875f126495f4
edle <eduard.lepner@powel.no>
2016-07-01 17:03:33 +0200

    Updated export function

--- MapCore/Controllers/MapServiceController.cs
+++ MapCore/Controllers/MapServiceController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Xml.Serialization;
@@ -46,26 +49,71 @@ namespace MapCore.Controllers
         }
 
         [HttpGet("export")]
-        public async Task<ExportParameters> Export([FromUri]ExportParameters exportParameters)
+        public async Task<IActionResult> Export([FromUri]ExportParameters exportParameters)
         {
             var wmsServiceReader = new FileWMSService(@"C:\Users\edle\Desktop\Capabilities.xml");
             var wmsServiceInfo = await wmsServiceReader.GetServiceInformation();
 
+            var esriServiceInfo = await Get();
 
-            var requestParams = new Dictionary<string, object>();
-            requestParams.Add("width", (int)exportParameters.Size.Values[0]);
-            requestParams.Add("height", (int)exportParameters.Size.Values[1]);
-            requestParams.Add("bbox", string.Join(",", exportParameters.Bbox.Values.Select(x => x.ToString(CultureInfo.InvariantCulture))));
-            var requestUriString = wmsServiceInfo.ServiceInfo.Resource.Url;
-            requestUriString += String.Join("&", requestParams.Select(x => x.Key + "=" + x.Value.ToString()));
+            var requestParams = new Dictionary<string, object>
+            {
+                {"width", (int) exportParameters.Size.Values[0]},
+                {"height", (int) exportParameters.Size.Values[1]},
+                {
+                    "bbox",
+                    string.Join(",", exportParameters.Bbox.Values.Select(x => x.ToString(CultureInfo.InvariantCulture)))
+                },
+                {"transparent", exportParameters.Transparent},
+                {"version", "1.1.1"},
+                {"request", "GetMap"},
+                {"service", "WMS"},
+                {"SRS", "EPSG:32632"},
+                {
+                    "layers","va_avl_line,va_line,va_cablepnt,va_cableline,va_mpnt,va_cpnt,va_cpnt_line,va_lequip,va_pequip,va_lineflowdir,va_freetxt,va_freepnt"
+                    //string.Join(",",
+                    //    exportParameters.Layers.Values.Select(layerId => esriServiceInfo.AllLayers[(int) layerId].Name))
+                },
+                {"format", "image/png"}
+            };
+
+            var requestUriString = wmsServiceInfo.ServiceInfo.Resource.Url + "?";
+            requestUriString += string.Join("&", requestParams.Select(x => x.Key + "=" + x.Value.ToString()));
+
+
             WebRequest webRequest = WebRequest.Create(requestUriString);
-            
+                        
             webRequest.Method = "GET";
-            //var result = await webRequest.GetResponseAsync();
+                        
+            //return Ok(requestUriString);
+            var wmsResult = await webRequest.GetResponseAsync();
+            var stream = wmsResult.GetResponseStream();
+            var dump = @"C:\temp\dump.png";
             
-            return exportParameters;
+            CopyStream(stream, dump);
+
+            return new StreamResult(stream);
+
         }
 
+
+
+        public void CopyStream(Stream stream, string destPath)
+        {
+            using (var fileStream = new FileStream(destPath, FileMode.Create, FileAccess.Write))
+            {
+                stream.CopyTo(fileStream);
+            }
+        }
+
+        private byte[] ReadFully(Stream input)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                input.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
         private void TraverseLayers(Models.WMS.Layer[] wmsLayers, List<Layer> esriLayers, Dictionary<int, Layer> allLayers, ref int count)
         {
             foreach (var wmsLayer in wmsLayers)
@@ -83,5 +131,27 @@ namespace MapCore.Controllers
                 }
             }
         }
+
+        class StreamResult : IActionResult
+        {
+            private readonly Stream _stream;
+
+            public StreamResult(Stream stream)
+            {
+                _stream = stream;
+            }
+
+            public Task ExecuteResultAsync(ActionContext context)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StreamContent(_stream)
+                };
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
+                return Task.FromResult(response);
+            }
+        }
     }
+
+    
 }
